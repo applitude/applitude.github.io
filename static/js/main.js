be0827fd6d21422f4f89e47aa2b59efe4e9dbae0
Roy Olav Purser <admin@royolav.net>
2016-09-12 17:32:45 +0200

    dynamic language change

--- static/js/main.js
+++ static/js/main.js
@@ -10,5 +10,43 @@ jQuery(document).ready(function(e) {
         }, 1e3)
     })
 });
-
-
+document.addEventListener('DOMContentLoaded', function(){
+    $("#norwegian-link").click(function(){
+        if(window.location.href.indexOf("/no/") !== -1){
+            return;
+        }
+        var newUrl = window.location.href.replace("\/en\/", "\/no\/");
+        if(newUrl === window.location.href){
+            newUrl = "/";
+        }
+        $.ajax({
+            type: "HEAD",
+            url: newUrl,
+            success: function(){
+                window.location.href = newUrl;
+            },
+            error: function(){
+                window.location.href = "/";
+            }
+        })
+    })
+    $("#english-link").click(function(){
+        if(window.location.href.indexOf("/en/") !== -1){
+            return;
+        }
+        var newUrl = window.location.href.replace("\/no\/", "\/en\/");
+        if(newUrl === window.location.href){
+            newUrl = "/en/";
+        }
+        $.ajax({
+            type: "HEAD",
+            url: newUrl,
+            success: function(){
+                window.location.href = newUrl;
+            },
+            error: function(){
+                window.location.href = "/en/";
+            }
+        })
+    })
+});
